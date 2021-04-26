19a4cd1a0aefaca0b1f338cae06e2c9891f8028b
Anto <aag@heyday.dk>
2021-04-26 15:27:12 +0200

    Added drawer functionality.

--- js/main.js
+++ js/main.js
@@ -1,25 +1,51 @@
-/* Dropdown START... */
+/* ==========================================================================
+   Drawer
+   ========================================================================== */
+/* When the user clicks on the button,
+toggle between hiding and showing the drawer */
+function toggleDrawer(event) {
+  if(event.target.matches(".toggle-drawer-button")) {
+    var drawerButtonTargetId = event.target.getAttribute("data-target-id");
+    event.target.classList.toggle("toggle-drawer-button--active");
+    document.getElementById(drawerButtonTargetId).classList.toggle("drawer--active");
+  }
+}
+
+// Close all drawers
+function closeAllDrawers() {
+  var drawers = document.getElementsByClassName("drawer");
+  var i;
+  for (i = 0; i < drawers.length; i++) {
+    var openDrawer = drawers[i];
+    if (openDrawer.classList.contains("drawer--active")) {
+      openDrawer.classList.remove("drawer--active");
+    }
+  }
+}
+
+/* ==========================================================================
+   Dropdown
+   ========================================================================== */
 
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function toggleDropdown(event) {
-    if(event.target.matches('.dropdown__button')) {
+    if(event.target.matches(".dropdown__button")) {
       event.target.classList.toggle("dropdown--active");
     }
 }
 
 // Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-    if (!event.target.matches('.dropdown__button')) {
-      var dropdowns = document.getElementsByClassName("dropdown__button");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('dropdown--active')) {
-          openDropdown.classList.remove('dropdown--active');
-        }
+window.addEventListener('click', function(event) {
+  if (!event.target.matches(".dropdown__button")) {
+    var dropdowns = document.getElementsByClassName("dropdown__button");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("dropdown--active")) {
+        openDropdown.classList.remove("dropdown--active");
       }
     }
   }
+});
 
-/* ...Dropdown END */
